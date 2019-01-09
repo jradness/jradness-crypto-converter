abf0928386301832cb6f7e7bfde017699d4eaf4f
Jason Brewer <jason@devslopes.com>
2019-01-09 17:24:07 -0600

    cleanup

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,6 @@
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
-
 export const environment = {
   production: false,
-  apikey: '9AB4C35C-283A-459E-AEBA-CF6AC5D0B453',
-  base_url: 'https://rest.coinapi.io/v1/exchangerate',
+  apikey: '',
+  base_url: '',
 };
 
